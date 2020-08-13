4f8f89541fa7fe2678264a8a19f65a6867c2ae84
LanHao <bigpangl@163.com>
2020-08-13 15:57:31 +0800

    编辑打包文件

--- setup.py
+++ setup.py
@@ -8,3 +8,16 @@ Python:     python3.6
 尝试打包成一个专门用于实现各种锁的模块
 
 """
+
+from setuptools import find_packages, setup
+
+setup(
+    name='pylocks',
+    version='1.0.0',
+    description="锁相关",
+    url="https://github.com/bigpangl/Locks",
+    author="LanHao",
+    author_email="bigpangl@163.com",
+    license="GPL-3.0",
+    packages=find_packages(),
+)
